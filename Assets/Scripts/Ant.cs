58073f9b8d7e84f63448711b30b8bf8530cdc9ae
Pykephobia <salil.niem@bumail.net>
2025-10-21 15:55:28 +0700

    ant can walk

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Ant : Enemy
@@ -9,10 +10,26 @@ public class Ant : Enemy
     void Start()
     {
         base.Initialize(20);
+        DamageHit = 20;
+        velocity = new Vector2(-1.0f, 0.0f);
     }
     public override void Behavior()
     {
-        throw new System.NotImplementedException();
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        
+    }
+    public void Flip()
+    {
+        velocity.x *= -1; //change direction of movement
+                          //Flip the image
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    private void FixedUpdate()
+    {
+        Behavior();
     }
 
     // Update is called once per frame
